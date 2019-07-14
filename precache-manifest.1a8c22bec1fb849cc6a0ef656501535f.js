1771408e077b12e0b959f73cce34c53c2d591161
Oliver Mulari <ormulari@gmail.com>
2019-07-14 15:07:34 +0300

    Updates

---
+++ precache-manifest.1a8c22bec1fb849cc6a0ef656501535f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102a89ad5f011114056cb098f95967bf",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "6284e0d3a24dfa9b154a",
+    "url": "/sudoku/static/css/main.8e4454bd.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/sudoku/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "6284e0d3a24dfa9b154a",
+    "url": "/sudoku/static/js/main.084ded39.chunk.js"
+  },
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  }
+]);
