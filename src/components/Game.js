a058895e150243a358313865a43227e5e5f21589
Oliver Mulari <ormulari@gmail.com>
2019-07-18 16:08:11 +0300

    prettier code

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,59 +1,67 @@
-import React from 'react';
-import '../App.css';
-import Board from './Board';
+import React from "react";
+import "../App.css";
+import Board from "./Board";
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          isCorrect: true,
-          isFilled: false,
-          isCompleted: false,
-          difficulty: this.props.difficulty
-        }
-        this.headerStyle = {
-          color: 'white',
-          textAlign: "center"
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCorrect: true,
+      isFilled: false,
+      isCompleted: false,
+      difficulty: this.props.difficulty
+    };
+    this.headerStyle = {
+      color: "white",
+      textAlign: "center"
+    };
+  }
 
-    setGameState= (isCorrect, isFilled) => {
+  setGameState = (isCorrect, isFilled) => {
+    this.setState(
+      {
+        isCorrect: isCorrect,
+        isFilled: isFilled
+      },
+      () => {
         this.setState({
-            isCorrect: isCorrect,
-            isFilled: isFilled
-        }, () => {
-            this.setState({
-                isCompleted: this.state.isCorrect && this.state.isFilled
-            })
-        })
-    }
+          isCompleted: this.state.isCorrect && this.state.isFilled
+        });
+      }
+    );
+  };
 
-    gameStyle = () => {
-        if (this.state.isFilled) {
-            if (this.state.isCompleted) {
-                return {background: 'green'}
-            } else {
-                return {background: 'red'}
-            }
-        } else {
-            return
-        }
+  gameStyle = () => {
+    if (this.state.isFilled) {
+      if (this.state.isCompleted) {
+        return { background: "green" };
+      } else {
+        return { background: "red" };
+      }
+    } else {
+      return;
     }
+  };
 
-    render() {
-        return (
-            <div className="sudoku-game" style={this.gameStyle()}>
-                <h1 style={this.headerStyle}>
-                    {this.state.isFilled ? (this.state.isCompleted ? 'Completed!' : 'Try again!') : 'Sudoku'}
-                </h1>
-                <Board 
-                tileHelper={this.props.tileHelper}
-                enableNotes={this.props.enableNotes}
-                difficulty= {this.props.difficulty}
-                setGameState={this.setGameState}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="sudoku-game" style={this.gameStyle()}>
+        <h1 style={this.headerStyle}>
+          {this.state.isFilled
+            ? this.state.isCompleted
+              ? "Completed!"
+              : "Try again!"
+            : "Sudoku"}
+        </h1>
+        <Board
+          tileHelper={this.props.tileHelper}
+          enableNotes={this.props.enableNotes}
+          difficulty={this.props.difficulty}
+          setGameState={this.setGameState}
+        />
+      </div>
+    );
+  }
 }
 
 export default Game;
--- src/components/OptionsRow.js
+++ src/components/OptionsRow.js
@@ -1,50 +1,51 @@
-import React from 'react';
-import ButtonTile from './ButtonTile';
+import React from "react";
+import ButtonTile from "./ButtonTile";
 
 class OptionsRow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.style = {
-            margin: '15px auto 0 auto',
-            backgroundColor: 'rgb(255, 255, 255)',
-            width: `${this.props.tileSize * 9 + 2}px`,
-            height: `${this.props.tileSize + 2}px`,
-            border: '1px white solid'
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.style = {
+      margin: "15px auto 0 auto",
+      backgroundColor: "rgb(255, 255, 255)",
+      width: `${this.props.tileSize * 9 + 2}px`,
+      height: `${this.props.tileSize + 2}px`,
+      border: "1px white solid"
+    };
+  }
 
-    setValue = (value, selected) => {
-        this.props.setValue(value, selected)
-    }
+  setValue = (value, selected) => {
+    this.props.setValue(value, selected);
+  };
 
-    figureTileColor = (val) => {
-        const selectedIsNull = this.props.selectedValue === 0
-        const isNote = this.props.selectedNotes.includes(val);
-        return isNote && selectedIsNull ? 'rgb(22, 237, 245)' : '';
-    }
+  figureTileColor = val => {
+    const selectedIsNull = this.props.selectedValue === 0;
+    const isNote = this.props.selectedNotes.includes(val);
+    return isNote && selectedIsNull ? "rgb(22, 237, 245)" : "";
+  };
 
-    render() {
-        let id = 0;
-        return (
-            <div style={this.style}>{
-                this.props.tiles.map((num) => {
-                    if (id === 9) id = 0;
-                    id++;
-                    return (
-                    <ButtonTile
-                    id={[this.props.id, id]}
-                    key={[this.props.id, id]}
-                    selected={this.props.selected}
-                    selectedValue={this.props.selectedValue}
-                    color={{backgroundColor: this.figureTileColor(id)}}
-                    value={num}
-                    setValue={this.setValue}
-                    size={this.props.tileSize}/>
-                )})
-            }
-            </div>
-        )
-    }
+  render() {
+    let id = 0;
+    return (
+      <div style={this.style}>
+        {this.props.tiles.map(num => {
+          if (id === 9) id = 0;
+          id++;
+          return (
+            <ButtonTile
+              id={[this.props.id, id]}
+              key={[this.props.id, id]}
+              selected={this.props.selected}
+              selectedValue={this.props.selectedValue}
+              color={{ backgroundColor: this.figureTileColor(id) }}
+              value={num}
+              setValue={this.setValue}
+              size={this.props.tileSize}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default OptionsRow;
